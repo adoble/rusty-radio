5ed0765bfa1584a8b2f74bbee022375f3a2c04ae
Andrew Doble <andrew.l.doble@gmail.com>
2024-11-12 16:49:34 +0100

    Toggle pin in task

--- software/controller/src/main.rs
+++ software/controller/src/main.rs
@@ -12,6 +12,7 @@
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
+use esp_hal::gpio::{AnyPin, Io, Level, Output};
 use esp_hal::timer::timg::TimerGroup;
 
 #[embassy_executor::task]
@@ -21,18 +22,30 @@ async fn run() {
         Timer::after(Duration::from_millis(1_000)).await;
     }
 }
+// Blink something
+#[embassy_executor::task]
+async fn toggle_pin(mut pin: Output<'static, AnyPin>) {
+    loop {
+        pin.toggle();
+        //esp_println::println!("Hello world from embassy using esp-hal-async!");
+        Timer::after(Duration::from_millis(50)).await;
+    }
+}
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
     esp_println::logger::init_logger_from_env();
-    let peripherals = esp_hal::init(esp_hal::Config::default());
-
     esp_println::println!("Init!");
+
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+    let pin = Output::new(io.pins.gpio1, Level::High);
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_hal_embassy::init(timg0.timer0);
 
     spawner.spawn(run()).ok();
+    spawner.spawn(toggle_pin(pin)).ok();
 
     loop {
         esp_println::println!("Bing!");
