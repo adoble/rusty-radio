6dd01f17e8375b2285be3b3b166d60bbab8992a1
Andrew Doble <andrew.l.doble@gmail.com>
2024-11-13 14:55:53 +0100

    Button input

--- software/controller/src/main.rs
+++ software/controller/src/main.rs
@@ -12,7 +12,7 @@
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
-use esp_hal::gpio::{AnyPin, Io, Level, Output};
+use esp_hal::gpio::{AnyPin, Input, Io, Level, Output, Pull};
 use esp_hal::timer::timg::TimerGroup;
 
 #[embassy_executor::task]
@@ -32,6 +32,14 @@ async fn toggle_pin(mut pin: Output<'static, AnyPin>) {
     }
 }
 
+#[embassy_executor::task]
+async fn button_monitor(mut pin: Input<'static, AnyPin>) {
+    loop {
+        pin.wait_for_falling_edge().await;
+        esp_println::println!("Button pressed!");
+    }
+}
+
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
     esp_println::logger::init_logger_from_env();
@@ -39,13 +47,15 @@ async fn main(spawner: Spawner) {
 
     let peripherals = esp_hal::init(esp_hal::Config::default());
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    let pin = Output::new(io.pins.gpio1, Level::High);
+    let output_toggle_pin = Output::new(io.pins.gpio2, Level::High);
+    let button_pin = Input::new(io.pins.gpio1, Pull::Up);
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_hal_embassy::init(timg0.timer0);
 
     spawner.spawn(run()).ok();
-    spawner.spawn(toggle_pin(pin)).ok();
+    spawner.spawn(toggle_pin(output_toggle_pin)).ok();
+    spawner.spawn(button_monitor(button_pin)).ok();
 
     loop {
         esp_println::println!("Bing!");
