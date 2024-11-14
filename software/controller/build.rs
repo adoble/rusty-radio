930e0382dd27494a915b4f172dbfeee24f155244
Andrew Doble <andrew.l.doble@gmail.com>
2024-11-14 17:05:34 +0100

    Added dependencies - code  builds

--- software/controller/build.rs
+++ software/controller/build.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("cargo:rustc-link-arg-bins=-Tlinkall.x");
+    // Uncomments this ljne leads to a linkage failure.
+    // Note that this (and other flags) are already set in .cargo/config.toml
+    //println!("cargo:rustc-link-arg-bins=-Tlinkall.x");
 }
