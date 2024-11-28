71990ed8e49989e10e826f79ef2914b09a0e81ee
Andrew Doble <andrew.l.doble@gmail.com>
2024-11-28 16:17:14 +0100

    sci_read passes first test

--- software/controller/vs1053-driver/src/lib.rs
+++ software/controller/vs1053-driver/src/lib.rs
@@ -1,5 +1,9 @@
 #![cfg_attr(not(test), no_std)]
 
+//! This driver uses no chip select (xdcs) pin as this is managed by `SpiDevice`.
+//! If the hal only provides an `SpiBus` then see [this](https://github.com/rust-embedded/embedded-hal/blob/master/docs/migrating-from-0.2-to-1.0.md#for-end-users) on how to convert a `SpiBus`
+//! into a `SpiDevice`
+
 use embedded_hal_async::digital::Wait;
 use embedded_hal_async::spi::{Operation, SpiDevice};
 
@@ -7,39 +11,31 @@ use embedded_hal::digital::OutputPin;
 
 const READ: u8 = 0b0000_0011;
 
-pub struct Vs1053Driver<SPI, MP3CS, XDCS, DREQ> {
-    spi: SPI,
+pub struct Vs1053Driver<SPI, MP3CS, DREQ> {
+    spi_device: SPI,
     mp3cs: MP3CS,
-    xdcs: XDCS,
     dreq: DREQ,
 }
 
-impl<SPI, MP3CS, XDCS, DREQ> Vs1053Driver<SPI, MP3CS, XDCS, DREQ>
+impl<SPI, MP3CS, DREQ> Vs1053Driver<SPI, MP3CS, DREQ>
 where
     SPI: SpiDevice,
     MP3CS: OutputPin,
-    XDCS: OutputPin,
     DREQ: Wait, // See https://docs.rs/embedded-hal-async/1.0.0/embedded_hal_async/digital/index.html
 {
-    pub fn new(spi: SPI, mp3cs: MP3CS, xdcs: XDCS, dreq: DREQ) -> Result<Self, DriverError> {
+    pub fn new(spi_device: SPI, mp3cs: MP3CS, dreq: DREQ) -> Result<Self, DriverError> {
         let driver = Vs1053Driver {
-            spi,
+            spi_device,
             mp3cs,
-            xdcs,
             dreq,
         };
         Ok(driver)
     }
 
     pub async fn sci_read(&mut self, addr: u8, data: &mut [u8]) -> Result<(), DriverError> {
-        // XDCS needs to be set low. This should be done by the transaction
-        // Send read instruction (0b0000 0011)
-        // then address (u8)
-        // then read data (2 x u8)
+        // Note: XDCS is managed by self.spi_device : SpiDevice
 
-        //let mut buf = [0; 2];
-
-        self.spi
+        self.spi_device
             .transaction(&mut [Operation::Write(&[READ, addr]), Operation::Read(data)])
             .await
             .map_err(|_| DriverError::SpiRead)?;
@@ -52,9 +48,9 @@ where
         todo!()
     }
 
-    // Destroys the driver and relases the  peripherals
-    pub fn release(self) -> (SPI, MP3CS, XDCS, DREQ) {
-        (self.spi, self.mp3cs, self.xdcs, self.dreq)
+    // Destroys the driver and releases the peripherals
+    pub fn release(self) -> (SPI, MP3CS, DREQ) {
+        (self.spi_device, self.mp3cs, self.dreq)
     }
 }
 
@@ -68,6 +64,8 @@ pub enum DriverError {
 mod tests {
     use super::*;
 
+    use embedded_hal_async::spi::SpiBus;
+    //use embedded_hal_bus::spi::{ExclusiveDevice, NoDelay};
     use embedded_hal_mock::eh1::digital::{
         Mock as PinMock, State as PinState, Transaction as PinTransaction,
     };
@@ -76,23 +74,31 @@ mod tests {
     #[async_std::test]
     //#[embassy_executor::test]
     async fn sci_read_test() {
+        // See https://github.com/rust-embedded/embedded-hal/blob/master/docs/migrating-from-0.2-to-1.0.md#for-end-users
+
         let spi_expectations = [
             SpiTransaction::transaction_start(),
             SpiTransaction::write_vec(vec![READ, 0x11]),
             SpiTransaction::read_vec(vec![0xAA, 0xBB]),
             SpiTransaction::transaction_end(),
         ];
-        let spi = SpiMock::new(&spi_expectations);
+        let spi_device = SpiMock::new(&spi_expectations); // Assuming this is a SpiBus
 
-        //let xdcs_expectations = [PinTransaction::set(PinState::Low)];
-        let xdcs_expectations: [PinTransaction; 0] = [];
-        let xdcs = PinMock::new(&xdcs_expectations);
+        // let xdcs_expectations = [
+        //     PinTransaction::set(PinState::Low),
+        //     PinTransaction::set(PinState::High),
+        // ];
+        // let xdcs_expectations: [PinTransaction; 0] = [];
+        // let xdcs = PinMock::new(&xdcs_expectations);
+
+        //let spi_device = ExclusiveDevice::new(spi_bus, xdcs, NoDelay).unwrap();
+
         // let mp3cs_expectations = [
         //     PinTransaction::set(PinState::Low),
         //     PinTransaction::set(PinState::High),
         // ];
         let mp3cs_expectations: [PinTransaction; 0] = [];
-        let mp3cs = PinMock::new(&xdcs_expectations);
+        let mp3cs = PinMock::new(&mp3cs_expectations);
 
         // let dreq_expectations = [
         //     PinTransaction::get(PinState::High),
@@ -101,16 +107,17 @@ mod tests {
         let dreq_expectations: [PinTransaction; 0] = [];
         let mut dreq = PinMock::new(&dreq_expectations);
 
-        let mut driver = Vs1053Driver::new(spi, mp3cs, xdcs, dreq).unwrap();
+        let mut driver = Vs1053Driver::new(spi_device, mp3cs, dreq).unwrap();
 
         let mut buf: [u8; 2] = [0; 2];
         let _ = driver.sci_read(0x11, &mut buf).await.unwrap();
 
-        let (mut spi, mut mp3cs, mut xdcs, mut dreq) = driver.release();
+        let (mut spi_device, mut mp3cs, mut dreq) = driver.release();
 
-        spi.done();
+        //let mut spi_bus = spi_device.bus();
+        spi_device.done();
         mp3cs.done();
-        xdcs.done();
+        //xdcs.done();
         dreq.done();
     }
 }
