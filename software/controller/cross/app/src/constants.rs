4c6781d740a2e27eb67645dd5d54725506a25af2
Andrew Doble <andrew.l.doble@gmail.com>
2025-07-02 12:54:37 +0200

    Station name length was not handiing real world conditions

--- software/controller/cross/app/src/constants.rs
+++ software/controller/cross/app/src/constants.rs
@@ -13,6 +13,6 @@ pub const NUMBER_SOCKETS_STACK_RESOURCES: usize = 3;
 //const_assert!(NUMBER_SOCKETS_STACK_RESOURCES >= NUMBER_SOCKETS_TCP_CLIENT_STATE);
 
 // Constants around the creation and reading of stations.
-pub const MAX_STATION_NAME_LEN: usize = 32;
+pub const MAX_STATION_NAME_LEN: usize = 40;
 pub const MAX_STATION_URL_LEN: usize = 256;
 pub const NUMBER_PRESETS: usize = 4;
