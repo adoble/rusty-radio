e1d3f6479f7f42cbf7d5ef90b1b3d8238a0ea1a9
Andrew Doble <andrew.l.doble@gmail.com>
2025-07-07 15:57:35 +0200

    Corrected range too large error

--- software/controller/cross/app/src/task/test_button_board.rs
+++ software/controller/cross/app/src/task/test_button_board.rs
@@ -23,35 +23,10 @@ pub async fn test_button_board(
     let mut last_button_pressed = Buttons::None;
 
     loop {
-        //interrupt_pin.wait_for_rising_edge().await;
-        //esp_println::println!("DEBUG: Interrupt pin low");
-
         let button_pressed = front_panel.button_pressed().await.unwrap();
 
         if button_pressed != last_button_pressed {
-            esp_println::println!("DEBUG: Button pressed = {:?}", button_pressed);
             last_button_pressed = button_pressed.clone();
         }
-
-        // Timer::after(Duration::from_millis(DEBOUNCE_DURATION)).await;
-
-        // let button_still_pressed: bool = match button_pressed {
-        //     Buttons::Button1 => front_panel.read_button(&button_pressed).await.unwrap(),
-        //     _ => false, //Ignore
-        // };
-
-        // if button_still_pressed {
-        //     esp_println::println!("DEBUG: Button pressed:  {:?}", button_pressed);
-        // }
-
-        // // Read the button to clear the interrupt
-        // let button_1_pressed = front_panel.read_button(Buttons::Button1).await.unwrap();
-
-        // // Debounce
-        // Timer::after(Duration::from_millis(DEBOUNCE_DURATION)).await;
-        // if button_1_pressed && front_panel.read_button(Buttons::Button1).await.unwrap() {
-        //     // Button is still pressed so acknowledge
-        //     esp_println::println!("DEBUG: Button 1 pressed");
-        // }
     }
 }
