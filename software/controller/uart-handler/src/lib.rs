6055976f9f4ffb038ef485e5eb85eb6116b7829f
Andrew Doble <andrew.l.doble@gmail.com>
2026-04-14 18:49:26 +0200

    Added Command and conversion

--- software/controller/uart-handler/src/lib.rs
+++ software/controller/uart-handler/src/lib.rs
@@ -89,3 +89,23 @@ where
 
     Ok(station_name)
 }
+
+#[derive(PartialEq, Debug)]
+pub enum Command {
+    Station,
+    Undefined,
+}
+
+impl Command {
+    pub fn to_string(&self) -> String<3> {
+        let mut command_string = String::<3>::new();
+
+        let s = match self {
+            Self::Station => "STA",
+            Self::Undefined => "",
+        };
+
+        command_string.push_str(s).unwrap();
+        command_string
+    }
+}
---
+++ software/controller/uart-handler/tests/command_tests.rs
@@ -0,0 +1,14 @@
+use uart_handler::Command;
+
+use heapless::String;
+
+#[test]
+fn test_conversion_to_str() {
+    let cmd = Command::Station;
+
+    let cmd_str: String<3> = cmd.to_string();
+
+    // let mut s = String::<3>::new();
+    // s.push_str("STA");
+    assert_eq!("STA", cmd_str);
+}
